282cece8b500ed48dfd39de5c2cad909a0c80906
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-27 12:03:21 +0300

    feat(4): style details page

--- src/pages/DetailsPage/index.tsx
+++ src/pages/DetailsPage/index.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import {
+ Card, CardContent, CardMedia, Container, Typography,
+} from '@mui/material';
 
 const DetailsPage = () => (
-  <Card sx={{ maxWidth: 345 }}>
-    <CardMedia
-      component="img"
-      height="140"
-      image="https://www.slimmingeats.com/blog/wp-content/uploads/2021/09/frozen-yoghurt-with-raspberries-and-white-chocolate-1-720x720.jpg"
-      alt="green iguana"
-    />
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        Lizard
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across
-        all continents except Antarctica
-      </Typography>
-    </CardContent>
-  </Card>
+  <Container>
+    <Card sx={{ maxWidth: 1000, mt: 10, mx: 'auto' }}>
+      <CardMedia
+        component="img"
+        height="500"
+        image="https://www.slimmingeats.com/blog/wp-content/uploads/2021/09/frozen-yoghurt-with-raspberries-and-white-chocolate-1-720x720.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
+  </Container>
 );
 
 export default DetailsPage;
