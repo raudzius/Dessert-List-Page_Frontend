d3995aeb6a664bb3aaf6f6cc1e8ad09f46adf093
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-29 01:04:32 +0300

    feat(8): details page based on fetched data

--- src/pages/DetailsPage/index.tsx
+++ src/pages/DetailsPage/index.tsx
@@ -2,27 +2,48 @@ import * as React from 'react';
 import {
  Card, CardContent, CardMedia, Container, Typography,
 } from '@mui/material';
+import { useParams } from 'react-router-dom';
 
-const DetailsPage = () => (
-  <Container>
-    <Card sx={{ maxWidth: 1000, mt: 10, mx: 'auto' }}>
-      <CardMedia
-        component="img"
-        height="500"
-        image="https://www.slimmingeats.com/blog/wp-content/uploads/2021/09/frozen-yoghurt-with-raspberries-and-white-chocolate-1-720x720.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-          across all continents except Antarctica
-        </Typography>
-      </CardContent>
-    </Card>
-  </Container>
-);
+const DetailsPage = () => {
+  const { id } = useParams();
+  const [dessert, setDessert] = React.useState({
+    id: 0,
+    name: '',
+    calories: 0,
+    fat: 0,
+    carbs: 0,
+    protein: 0,
+    image: '',
+    details: '',
+  });
+  React.useEffect(() => {
+    (async () => {
+      const res = await fetch(`http://localhost:4000/details/${id}`);
+      const data = await res.json();
+      setDessert(data);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Card sx={{ maxWidth: 1000, mt: 10, mx: 'auto' }}>
+        <CardMedia
+          component="img"
+          height="500"
+          image={dessert.image}
+          alt="dessert image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {dessert.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {dessert.details}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
 
 export default DetailsPage;
