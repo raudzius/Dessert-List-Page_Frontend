a26e2e98ea047be6c55838667064cc9bebd72177
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-29 16:51:06 +0300

    chore(9): eslint allows _id

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-    env: {
-        'browser': true,
-        'es2021': true
-    },
-    extends: [
-        'airbnb',
-        'airbnb-typescript',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'airbnb-typescript'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/button-has-type': [0],
+    'react/prop-types': [0],
+    'react/require-default-props': [0],
+    'react/jsx-props-no-spreading': [0],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json'
-    },
-    rules: {
-        'react/button-has-type': [0],
-        'react/prop-types': [0],
-        'react/require-default-props': [0],
-        'react/jsx-props-no-spreading': [0],
-        'react/function-component-definition': [2, {
-            namedComponents: 'arrow-function',
-            unnamedComponents: 'arrow-function',
-        }],
-        'react/jsx-max-props-per-line': [2, { maximum: { single: 88, multi: 1 } }],
-        '@typescript-eslint/indent': [0]
-    }
-}
+    'react/jsx-max-props-per-line': [2, { maximum: { single: 88, multi: 1 } }],
+    '@typescript-eslint/indent': [0],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
+};
